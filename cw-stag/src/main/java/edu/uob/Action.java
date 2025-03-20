d1301b280810e8cdff2193dd21c4ce7e10244cac
rohit <rohitbhagatkar940@gmail.com>
2025-03-20 23:41:33 +0000

    Action Class implement!

---
+++ cw-stag/src/main/java/edu/uob/Action.java
@@ -0,0 +1,13 @@
+package edu.uob;
+
+import java.util.HashSet;
+
+public class Action
+{
+    HashSet<String> entitiesInvolved;
+    HashSet<String> entitiesUsed;
+    HashSet<String> entitiesMade;
+    HashSet<String> triggers;
+    String actionStatement;
+
+}
